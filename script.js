8d02eaafa32c952510abd2751238e8641a7d78e2
micpot <32093024+micpot@users.noreply.github.com>
2018-10-04 15:43:08 +0200

    delete console.log in script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 var mobileRun = document.querySelector('.mobile-menu-title');
-console.log("mobileRun: ", mobileRun);
 
 mobileRun.addEventListener('click', use);
 
 function use() {
     var menuNode = document.querySelector('.menu');
 
-    console.log('run: ', menuNode);
 
     if (menuNode.classList.contains('open')) {
         menuNode.classList.remove('open');
